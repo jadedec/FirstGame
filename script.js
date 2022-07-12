ed1e77da3731bdfa718ca89c59f789ef5274d803
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-12 17:15:24 +0100

    coin animation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const main = document.querySelector("main");
 const scoreBox = document.querySelector(".result");
 const button = document.querySelector(".button");
 const timer = document.querySelector(".timer");
+const coin = document.querySelector(".coin");
 
 let isStart = false;
 let difficulty = 1; //default difficulty
@@ -15,6 +16,7 @@ const gameStart = () => {
     if (!isStart) {
         isStart = true;
         score = 0;
+        coin.classList.add("coin-" + difficulty);
         //build a counting down timer
         // Update the count down every 1 second
         let timeNumber = 30;
@@ -24,8 +26,7 @@ const gameStart = () => {
             if (timeNumber <= 0) {
                 //stop running the function
                 clearInterval(numberId);
-                isStart = false;
-                alert('Game Over');
+                clear();
             }
         }, 1000)
     }
@@ -35,36 +36,27 @@ button.addEventListener('click', gameStart);
 
 // Difficulty
 let radios = document.querySelectorAll(".difficultyButton");
+const difficultyChosen = (event) => {
+    difficulty = event.target.value;
+}
 radios.forEach(radio => {
     //use"change" instead of "click" to avoid running repeatly
-    radio.addEventListener("change", () => {
-        difficulty = radio.value;
-    })
+    radio.addEventListener("change", difficultyChosen)
 })
 
 
-
-//add coin to the game, 
-const addElement = () => {
-    //add a new img element
-    let icon = document.createElement("img");
-    icon.src = "./Golden Coin.jpeg";
-    //set coin type based on difficulty
-    icon.classList.add('coin' + difficulty);
-    //give it a random start position
-    icon.style.left = Math.random() * 730 + 'px'
-    main.appendChild(icon);
-
-
-    //when clicked, remove the icon , means caught
-    icon.addEventListener('click', () => {
-        main.removeChild(icon);
-        score += 10 * difficulty;
-        scoreBox.innerHTML = score;
-    })
+const clear = () => {
+    isStart = false;
+    difficulty = 1;
+    score = 0;
+    coin.classList.remove("coin-" + difficulty);
 }
 
-
+//when clicked, remove the icon , means caught
+coin.addEventListener('click', () => {
+    score += 10 * difficulty;
+    scoreBox.innerHTML = score;
+})
 
 
 
